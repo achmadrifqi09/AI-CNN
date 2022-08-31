9d1c2c758f5c611f6ef240509d711b9453839322
Achmad Rifqi Rosadi <55131961+achmadrifqi09@users.noreply.github.com>
2022-08-31 17:34:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,18 +40,3 @@ def send_uploaded_image(filename=''):
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-# from flask import Flask, render_template
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def main():
-#     return render_template("index.html")
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
-
